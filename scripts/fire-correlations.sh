fdc2fcc0690691add856af321971705372da42f5
Mitchell R. Vollger <mvollger@gmai.com>
2024-08-16 18:31:56 -0700

    update

--- scripts/fire-correlations.sh
+++ scripts/fire-correlations.sh
@@ -14,7 +14,7 @@ rb bl -r Tables/joint.peaks.bed
 
 cat Tables/joint.peaks.bed |
     bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score ../FIREv2.0/results/GM12878/FDR-peaks/FDR.track.bed.gz ) |
-    bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score FIRE/results/ONT_heuristic_full/FDR-peaks/FDR.track.bed.gz) |
+    bedmap --ec --echo --max --delim '\t' - <(hck -z -f 1-4 -F score FIRE/results/ONT_modkit/FDR-peaks/FDR.track.bed.gz) |
     cat \
         >>"./Tables/fire-correlations.tbl"
 head ./Tables/fire-correlations.tbl
